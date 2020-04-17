aafd173d4dff25418e4334ed437d6f426267e932
Hellowalls1 <thesingingbutcher@gmail.com>
2020-04-17 14:07:13 -0500

    part 1

---
+++ scripts/PotteryList.js
@@ -0,0 +1,28 @@
+const potteryToSell = [potteryObject]
+
+
+const newPotteryObject = (createPottery())
+
+export let createPottery = (shape, weight, height) => {
+    const pottery =  {
+        id: 1,
+        Shape: shape,
+        Weight: weight,
+        Height: height
+    }
+        return pottery
+    
+}
+
+
+export const potteryKiln = (potteryObject, kilnTemperature) => {
+    if (kilnTemperature > 2200) {
+        return cracked = true
+    } else {
+        if (kilnTemperature <= 2200 )
+        return cracked = false
+    }
+}
+
+potteryKiln
+
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+import { createPottery } from "./PotteryList.js"
+
+createPottery()
+
